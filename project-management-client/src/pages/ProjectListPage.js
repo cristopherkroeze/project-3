456ee4d8d9d7451ec5663f90935351766e927bb7
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-05-16 17:22:11 -0400

    lesson done

---
+++ project-management-client/src/pages/ProjectListPage.js
@@ -0,0 +1,50 @@
+// src/pages/ProjectListPage.js
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import AddProject from "../components/AddProject";
+import ProjectCard from "../components/ProjectCard";
+
+import { get, post } from "../services/authService";
+
+const API_URL = "http://localhost:4000";
+
+
+function ProjectListPage() {
+  const [projects, setProjects] = useState([]);
+
+  const getAllProjects = () => {
+
+    const storedToken = localStorage.getItem("authToken");
+
+    get('/projects')
+
+      .then((response) => setProjects(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  // We set this effect will run only once, after the initial render
+  // by setting the empty dependency array - []
+  useEffect(() => {
+    getAllProjects();
+  }, [] );
+
+  
+  return (
+    <div className="ProjectListPage">
+
+        <AddProject refreshProjects={getAllProjects} />
+      
+        {projects.map((project) => {
+          return (
+            <ProjectCard key={project._id} {...project} />
+          );
+        })}     
+       
+    </div>
+  );
+}
+
+export default ProjectListPage;
---
+++ project-management-server/models/User.js
@@ -0,0 +1,26 @@
+// models/User.model.js
+
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        email: { 
+            type: String, 
+            unique: true, 
+            required: true 
+            },
+        password: { 
+            type: String, 
+            required: true 
+            },
+        name: { 
+            type: String, 
+            required: true 
+            },
+    },
+    {
+        timeseries: true,
+        timestamps: true
+    });
+
+module.exports = model("User", userSchema);
